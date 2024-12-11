02e9b9ca6ed60c7d9ecb930fd354cf3ebc2ebd3b
Fahri Gedik <fahrican9978@gmail.com>
2024-12-11 14:10:44 +0300

    Add framework dependency

--- AuthServer/AuthServer.API/obj/Debug/net8.0/AuthServer.API.AssemblyInfo.cs
+++ AuthServer/AuthServer.API/obj/Debug/net8.0/AuthServer.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9643cf375e083eed85fb38858be40cbe718ba351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e9d6427fe340079ebcee5c6ccdd3793e69b324")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
