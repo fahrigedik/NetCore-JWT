52b6a14c164a31881c0d62de464155f50dbcada4
Fahri Gedik <fahrican9978@gmail.com>
2024-12-11 13:59:12 +0300

    Create shared library

--- AuthServer/AuthServer.API/obj/Debug/net8.0/AuthServer.API.AssemblyInfo.cs
+++ AuthServer/AuthServer.API/obj/Debug/net8.0/AuthServer.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659229c0afee422188fb047615a7ee61273bfa5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d07c453ad40d32803cd7bdc808de813dcbad5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
