3fb8fb1209a9f2901c80d7eec6cfe5b56bf865d7
Fahri Gedik <fahrican9978@gmail.com>
2024-12-12 23:16:21 +0300

    Unit of work

---
+++ AuthServer/AuthServer.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using AuthServer.Core.UnitOfWork;
+
+namespace AuthServer.Data.UnitOfWork;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+
+    public void SaveChanges()
+    {
+        context.SaveChanges();
+    }
+
+    public async Task RollbackAsync()
+    {
+        await context.Database.RollbackTransactionAsync();
+    }
+
+    public void Rollback()
+    {
+        context.Database.RollbackTransaction();
+    }
+}
+
